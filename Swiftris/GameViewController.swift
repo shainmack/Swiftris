78963359434542d80e9abf993003193c99a2d3c8
Shain Toth <shaintoth@icloud.com>
2020-02-04 23:43:05 -0500

    Initial Creation

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -14,40 +14,53 @@ class GameViewController: UIViewController {
     
     var scene: GameScene!
     
+    var swiftris:Swiftris!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let skView = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                skView.presentScene(scene)
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        
+        scene.tick = didTick
+        
+        swiftris = Swiftris()
+        
+        swiftris.beginGame()
+        
+        
+        // Present the scene
+        skView.presentScene(scene)
+        
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        scene.addPreviewShapeToScene(shape: swiftris.nextShape!) {
+            self.swiftris.nextShape?.moveTo(column: StartingColumn, row: StartingRow)
+            self.scene.movePreviewShape(shape: self.swiftris.nextShape!) {
+                let nextShapes = self.swiftris.newShape()
+                self.scene.startTicking()
+                self.scene.addPreviewShapeToScene(shape: nextShapes.nextShape!){}
             }
-            
-            skView.ignoresSiblingOrder = true
-            
-            skView.showsFPS = true
-            skView.showsNodeCount = true
         }
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
+    
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    func didTick() {
+        swiftris.fallingShape?.lowerShapeByOneRow()
+        
+        scene.redrawShape(shape: swiftris.fallingShape!, completion: {})
+    }
 }
+
