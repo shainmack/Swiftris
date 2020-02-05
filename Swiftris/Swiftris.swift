d7d650605bb08e3e508a4ad6f7916fbd00a6edb9
Shain Toth <shaintoth@icloud.com>
2020-02-05 00:59:06 -0500

    Added Swiftris delegate and shape boundary detection

--- Swiftris/Swiftris.swift
+++ Swiftris/Swiftris.swift
@@ -15,10 +15,31 @@ let StartingRow = 0
 let PreviewColumn = 12
 let PreviewRow = 1
 
+protocol SwiftrisDelegate {
+    // Invoked when the current round of Swiftris ends
+    func gameDidEnd(swiftris: Swiftris)
+
+    // Invoked after a new game has begun
+    func gameDidBegin(swiftris: Swiftris)
+
+    // Invoked when the falling shape has become part of the game board
+    func gameShapeDidLand(swiftris: Swiftris)
+
+    // Invoked when the falling shape has changed its location
+    func gameShapeDidMove(swiftris: Swiftris)
+
+    // Invoked when the falling shape has changed its location after being dropped
+    func gameShapeDidDrop(swiftris: Swiftris)
+
+    // Invoked when the game has reached a new level
+    func gameDidLevelUp(swiftris: Swiftris)
+}
+
 class Swiftris {
     var blockArray: Array2D<Block>
     var nextShape: Shape?
     var fallingShape: Shape?
+    var delegate:SwiftrisDelegate?
     
     init() {
         fallingShape = nil
@@ -30,6 +51,7 @@ class Swiftris {
         if (nextShape == nil) {
             nextShape = Shape.random(startingColumn: PreviewColumn, startingRow: PreviewRow)
         }
+        delegate?.gameDidBegin(swiftris: self)
     }
     
     func newShape() -> (fallingShape:Shape?, nextShape:Shape?){
@@ -37,6 +59,35 @@ class Swiftris {
         nextShape = Shape.random(startingColumn: PreviewColumn, startingRow: PreviewRow)
         fallingShape?.moveTo(column: StartingColumn, row: StartingRow)
         
+        guard detectIllegalPlacement() == false else {
+            nextShape = fallingShape
+            
+            nextShape!.moveTo(column: PreviewColumn, row: PreviewRow)
+            
+            endGame()
+            
+            return(nil, nil)
+        }
+        
         return (fallingShape, nextShape)
+    }
+    
+    func detectIllegalPlacement() -> Bool {
+        guard let shape = fallingShape else {
+            return false
+        }
+        
+        for block in shape.blocks {
+            if block.column < 0 || block.column >= NumColumns || block.row < 0 || block.row >= NumRows{
+                return true
+            } else if blockArray[block.column, block.row] != nil {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func endGame() {
+        delegate?.gameDidEnd(swiftris: self)
     }
 }
